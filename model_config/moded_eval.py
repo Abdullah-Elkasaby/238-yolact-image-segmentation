2dab33c92481b4d7b1fddc6d4c42388902f0b40a
Abdullah Elkasaby <abdullah-elkasaby@users.noreply.github.com>
2023-05-18 15:53:59 +0300

    moved files into model_config dir

--- model_config/moded_eval.py
+++ model_config/moded_eval.py
@@ -1,11 +1,11 @@
 from data import COCODetection, get_label_map, MEANS, COLORS
-from yolact import Yolact
 from utils.augmentations import BaseTransform, FastBaseTransform, Resize
 from utils.functions import MovingAverage, ProgressBar
 from layers.box_utils import jaccard, center_size
 from utils import timer
 from utils.functions import SavePath
 from layers.output_utils import postprocess, undo_image_transformation
+from model_config.yolact import Yolact
 import pycocotools
 
 from data import cfg, set_cfg, set_dataset
@@ -29,96 +29,61 @@ import pdb
 import matplotlib.pyplot as plt
 import cv2
 
-def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
 
-def parse_args(argv=None):
-    parser = argparse.ArgumentParser(
-        description='YOLACT COCO Evaluation')
-    parser.add_argument('--trained_model',
-                        default='weights/ssd300_mAP_77.43_v2.pth', type=str,
-                        help='Trained state_dict file path to open. If "interrupt", this will open the interrupt file.')
-    parser.add_argument('--top_k', default=5, type=int,
-                        help='Further restrict the number of predictions to parse')
-    parser.add_argument('--cuda', default=True, type=str2bool,
-                        help='Use cuda to evaulate model')
-    parser.add_argument('--fast_nms', default=True, type=str2bool,
-                        help='Whether to use a faster, but not entirely correct version of NMS.')
-    parser.add_argument('--display_masks', default=True, type=str2bool,
-                        help='Whether or not to display masks over bounding boxes')
-    parser.add_argument('--display_bboxes', default=True, type=str2bool,
-                        help='Whether or not to display bboxes around masks')
-    parser.add_argument('--display_text', default=True, type=str2bool,
-                        help='Whether or not to display text (class [score])')
-    parser.add_argument('--display_scores', default=True, type=str2bool,
-                        help='Whether or not to display scores in addition to classes')
-    parser.add_argument('--display', dest='display', action='store_true',
-                        help='Display qualitative results instead of quantitative ones.')
-    parser.add_argument('--shuffle', dest='shuffle', action='store_true',
-                        help='Shuffles the images when displaying them. Doesn\'t have much of an effect when display is off though.')
-    parser.add_argument('--ap_data_file', default='results/ap_data.pkl', type=str,
-                        help='In quantitative mode, the file to save detections before calculating mAP.')
-    parser.add_argument('--resume', dest='resume', action='store_true',
-                        help='If display not set, this resumes mAP calculations from the ap_data_file.')
-    parser.add_argument('--max_images', default=-1, type=int,
-                        help='The maximum number of images from the dataset to consider. Use -1 for all.')
-    parser.add_argument('--output_coco_json', dest='output_coco_json', action='store_true',
-                        help='If display is not set, instead of processing IoU values, this just dumps detections into the coco json file.')
-    parser.add_argument('--bbox_det_file', default='results/bbox_detections.json', type=str,
-                        help='The output file for coco bbox results if --coco_results is set.')
-    parser.add_argument('--mask_det_file', default='results/mask_detections.json', type=str,
-                        help='The output file for coco mask results if --coco_results is set.')
-    parser.add_argument('--config', default=None,
-                        help='The config object to use.')
-    parser.add_argument('--output_web_json', dest='output_web_json', action='store_true',
-                        help='If display is not set, instead of processing IoU values, this dumps detections for usage with the detections viewer web thingy.')
-    parser.add_argument('--web_det_path', default='web/dets/', type=str,
-                        help='If output_web_json is set, this is the path to dump detections into.')
-    parser.add_argument('--no_bar', dest='no_bar', action='store_true',
-                        help='Do not output the status bar. This is useful for when piping to a file.')
-    parser.add_argument('--display_lincomb', default=False, type=str2bool,
-                        help='If the config uses lincomb masks, output a visualization of how those masks are created.')
-    parser.add_argument('--benchmark', default=False, dest='benchmark', action='store_true',
-                        help='Equivalent to running display mode but without displaying an image.')
-    parser.add_argument('--no_sort', default=False, dest='no_sort', action='store_true',
-                        help='Do not sort images by hashed image ID.')
-    parser.add_argument('--seed', default=None, type=int,
-                        help='The seed to pass into random.seed. Note: this is only really for the shuffle and does not (I think) affect cuda stuff.')
-    parser.add_argument('--mask_proto_debug', default=False, dest='mask_proto_debug', action='store_true',
-                        help='Outputs stuff for scripts/compute_mask.py.')
-    parser.add_argument('--no_crop', default=False, dest='crop', action='store_false',
-                        help='Do not crop output masks with the predicted bounding box.')
-    parser.add_argument('--image', default=None, type=str,
-                        help='A path to an image to use for display.')
-    parser.add_argument('--images', default=None, type=str,
-                        help='An input folder of images and output folder to save detected images. Should be in the format input->output.')
-    parser.add_argument('--video', default=None, type=str,
-                        help='A path to a video to evaluate on. Passing in a number will use that index webcam.')
-    parser.add_argument('--video_multiframe', default=1, type=int,
-                        help='The number of frames to evaluate in parallel to make videos play at higher fps.')
-    parser.add_argument('--score_threshold', default=0, type=float,
-                        help='Detections with a score under this threshold will not be considered. This currently only works in display mode.')
-    parser.add_argument('--dataset', default=None, type=str,
-                        help='If specified, override the dataset specified in the config with this one (example: coco2017_dataset).')
-    parser.add_argument('--detect', default=False, dest='detect', action='store_true',
-                        help='Don\'t evauluate the mask branch at all and only do object detection. This only works for --display and --benchmark.')
+class YOLACTConfig:
+    def __init__(self, top_k=5, cuda=True, fast_nms=True,
+                 display_masks=True, display_bboxes=True, display_text=True, display_scores=True, display=False,
+                 shuffle=False, ap_data_file='results/ap_data.pkl', resume=False, max_images=-1,
+                 output_coco_json=False, bbox_det_file='results/bbox_detections.json',
+                 mask_det_file='results/mask_detections.json', config=None, output_web_json=False,
+                 web_det_path='web/dets/', no_bar=False, display_lincomb=False, benchmark=False, no_sort=False,
+                 seed=None, mask_proto_debug=False, crop=True, image=None, images=None, video=None, video_multiframe=1,
+                 score_threshold=0, dataset=None, detect=False):
+        
 
-    parser.set_defaults(no_bar=False, display=False, resume=False, output_coco_json=False, output_web_json=False, shuffle=False,
-                        benchmark=False, no_sort=False, no_hash=False, mask_proto_debug=False, crop=True, detect=False)
+        self.trained_model = "weights/yolact_base_54_800000.pth"
+        self.top_k = top_k
+        self.cuda = cuda
+        self.fast_nms = fast_nms
+        self.display_masks = display_masks
+        self.display_bboxes = display_bboxes
+        self.display_text = display_text
+        self.display_scores = display_scores
+        self.display = display
+        self.shuffle = shuffle
+        self.ap_data_file = ap_data_file
+        self.resume = resume
+        self.max_images = max_images
+        self.output_coco_json = output_coco_json
+        self.bbox_det_file = bbox_det_file
+        self.mask_det_file = mask_det_file
+        self.config = config
+        self.output_web_json = output_web_json
+        self.web_det_path = web_det_path
+        self.no_bar = no_bar
+        self.display_lincomb = display_lincomb
+        self.benchmark = benchmark
+        self.no_sort = no_sort
+        self.seed = seed
+        self.mask_proto_debug = mask_proto_debug
+        self.crop = crop
+        self.image = image
+        self.images = images
+        self.video = video
+        self.video_multiframe = video_multiframe
+        self.score_threshold = score_threshold
+        self.dataset = dataset
+        self.detect = detect
 
-    global args
-    args = parser.parse_args(argv)
 
-    if args.output_web_json:
-        args.output_coco_json = True
-    
-    if args.seed is not None:
-        random.seed(args.seed)
+global args
+# args = parser.parse_args(argv)
+args = YOLACTConfig()
+if args.output_web_json:
+    args.output_coco_json = True
+
+if args.seed is not None:
+    random.seed(args.seed)
 
 iou_thresholds = [x / 100 for x in range(50, 100, 5)]
 coco_cats = {} # Call prep_coco_cats to fill this
@@ -973,70 +938,10 @@ def print_maps(all_maps):
     for iou_type in ('box', 'mask'):
         print(make_row([iou_type] + ['%.2f' % x for x in all_maps[iou_type].values()]))
     print(make_sep(len(all_maps['box']) + 1))
-    print()
 
-
-
-if __name__ == '__main__':
-    parse_args()
-    if args.config is not None:
-        set_cfg(args.config)
-
-    if args.trained_model == 'interrupt':
-        args.trained_model = SavePath.get_interrupt('weights/')
-    elif args.trained_model == 'latest':
-        args.trained_model = SavePath.get_latest('weights/', cfg.name)
-
-    if args.config is None:
-        model_path = SavePath.from_str(args.trained_model)
-        # TODO: Bad practice? Probably want to do a name lookup instead.
-        args.config = model_path.model_name + '_config'
-        print('Config not specified. Parsed %s from the file name.\n' % args.config)
-        set_cfg(args.config)
-
-    if args.detect:
-        cfg.eval_mask_branch = False
-
-    if args.dataset is not None:
-        set_dataset(args.dataset)
-
-    with torch.no_grad():
-        if not os.path.exists('results'):
-            os.makedirs('results')
-
-        if args.cuda:
-            # cudnn.benchmark = True
-            # cudnn.fastest = True
-            # torch.set_default_tensor_type('torch.cuda.FloatTensor')
-            torch.set_default_tensor_type('torch.FloatTensor')
-        else:
-            torch.set_default_tensor_type('torch.FloatTensor')
-
-        if args.resume and not args.display:
-            with open(args.ap_data_file, 'rb') as f:
-                ap_data = pickle.load(f)
-            calc_map(ap_data)
-            exit()
-
-        if args.image is None and args.video is None and args.images is None:
-            dataset = COCODetection(cfg.dataset.valid_images, cfg.dataset.valid_info,
-                                    transform=BaseTransform(), has_gt=cfg.dataset.has_gt)
-            prep_coco_cats()
-        else:
-            dataset = None        
-
-        print('Loading model...', end='')
-        net = Yolact()
-        net.load_weights(args.trained_model)
-        # pdb.set_trace()
-        net.eval()
-        print(' Done.')
-
-        if args.cuda:
-            # net = net.cuda()
-            net = net
-
-        evaluate(net, dataset)
-        # evalimage(net, "weights/yolact_base_54_800000.pth", "image=test_img.jpg:output_image.jpg")
-
-
+# print('Loading model...', end='')
+net = Yolact()
+net.load_weights(args.trained_model)
+# pdb.set_trace()
+net.eval()
+# print(' Done.')
